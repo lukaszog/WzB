4f574955d69ac6c7686e1c1cac2240f6ab82764f
MarcinLenda <777marcinlenda777@gmail.com>
2016-11-28 18:44:06 +0100

    added sleep

--- src/main/java/pl/lenda/marcin/wzb/controller/ClientController.java
+++ src/main/java/pl/lenda/marcin/wzb/controller/ClientController.java
@@ -27,7 +27,7 @@ public class ClientController {
     private ConvertTo convertTo;
 
 
-    @CrossOrigin(origins = "http://localhost:8080")
+    @CrossOrigin(origins = "http://52.39.52.69:8080")
     @RolesAllowed("ADMIN")
     @RequestMapping(value = "/save_client", method = RequestMethod.POST)
     public
@@ -55,7 +55,7 @@ public class ClientController {
 
     }
 
-    @CrossOrigin(origins = "http://localhost:8080")
+    @CrossOrigin(origins = "http://52.39.52.69:8080")
     @RequestMapping(value = "/delete_client", method = RequestMethod.DELETE)
     public void deleteClient(@RequestBody ClientFindDto clientFindDto){
         ClientAccount clientAccount = clientAccountService.findByClientNameAndNumber(clientFindDto.getName(),
@@ -63,7 +63,7 @@ public class ClientController {
         clientAccountService.deleteAccountClient(clientAccount);
     }
 
-    @CrossOrigin(origins = "http://localhost:8080")
+    @CrossOrigin(origins = "http://52.39.52.69:8080")
     @RequestMapping(value = "/all_client", method = RequestMethod.GET)
     public List<ClientAccount> allClientAccount() {
         List<ClientAccount> listClient = new ArrayList<>();
--- src/main/java/pl/lenda/marcin/wzb/service/scheduled_task/MyScheduledTasks.java
+++ src/main/java/pl/lenda/marcin/wzb/service/scheduled_task/MyScheduledTasks.java
@@ -45,7 +45,7 @@ public class MyScheduledTasks {
     }
 
     @Scheduled(fixedRate = 10000000)
-    public void sendMailToCustomers() throws MessagingException {
+    public void sendMailToCustomers() throws MessagingException, InterruptedException {
 
         List<UserAccount> userAccounts = userAccountService.findAllAccount();
         List<TraderAccount> traderAccounts = new ArrayList<>();
@@ -76,7 +76,7 @@ public class MyScheduledTasks {
         String line5 = "";
         String endContent = " </table>\n" +
                 "    <p style=\"font-size: 17px;\">Gdybyś miał zamiar zrobić korektę do któregoś z dokumentów poinformuj nas o tym na: <a\n" +
-                "            href=\"http://localhost:8080/#/login\">\n" +
+                "            href=\"http://52.39.52.69:8080/#/login\">\n" +
                 "        <input style=\"width:150px; margin-top: 30px;\" type=\"button\" value=\"WzB\">\n" +
                 "    </a></p>\n" +
                 "\n" +
@@ -292,6 +292,7 @@ public class MyScheduledTasks {
                 content = "";
                 content2 = "";
                 howManyDocumentsToSend = 0;
+                Thread.sleep(10000);
 
             }
         }
